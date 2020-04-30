b230305a79944e57c5cc4edf421580524bcee7ab
BossaMelon <wyh453255505@gmail.com>
2020-04-30 21:06:05 +0200

    add git ignore

--- main.py
+++ main.py
@@ -1,57 +1,123 @@
-import time
 from pathlib import Path
 
 import requests
 from bs4 import BeautifulSoup
+import time
+from util.get_cookies import get_cookies
 from util.segment import multiThreadDownload
 
-txt_path = Path() / 'resource' / 'data_structure' / 'download_link' / 'TheUltimateDataStructures&Algorithms:Part1.txt'
-url_list = []
-with open(txt_path) as fp:
-    for line in fp:
-        url_list.append(line.split('\n')[0])
+cookies = get_cookies()
+headers = {'Cookie': cookies}
+homepage = 'https://codewithmosh.com/courses/enrolled/240431'
+response_home = requests.get(homepage, headers=headers)
+soup_home = BeautifulSoup(response_home.text, 'html.parser')
+
+course_dict = {}
+
+for i in soup_home.find_all('div', {'class': 'row'}):
+    course_name = i.find('div', {'class': 'course-listing-title'}).string
+    course_name = course_name.split('\n')[1].split('\n')[0].split('(')[0].strip()
+    course_url = i.find('a').get('href')
+    course_url = 'https://' + 'codewithmosh.com' + course_url
+    course_dict.update({course_name: course_url})
+
+print('avalible courses')
+print(20 * '*')
+course_no_dict = {}
+for num, key in enumerate(course_dict):
+    print(f'{num}: {key}')
+    course_no_dict.update({num: key})
+print(20 * '*')
+
+# course_no = int(input('Choose a course by number: '))
+course_no = 23
+assert course_no in range(len(course_dict))
+
+course_title = course_no_dict[course_no]
+print(f'"{course_title}" is chosen')
+# print(f'url: {course_dict[course_no_dict[course_no]]}')
+print('Searching download infomation...')
+print()
+
+# ****************
+url = course_dict[course_no_dict[course_no]]
+
+# http get
+response = requests.get(url, headers=headers)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+info_dic = {}
+
+for section in soup.find_all('div', {'class': 'col-sm-12 course-section'}):
+    sectionTitle = section.find('div', {'class': 'section-title'})
+    section_name = list(sectionTitle.strings)[3].split('\n')[1].split('\n')[0].split('(')[0].strip()
+
+    section_info_list = []
+
+    print(f'Section: {section_name}')
+
+    for lectureUrl in section.find_all('a'):
+        url = 'https://' + 'codewithmosh.com' + lectureUrl.get('href')
+        response = requests.get(url, headers=headers)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        for link in soup.find_all('a', {'class': 'download'}):
+            file_url = link.get('href')
+            header = requests.head(file_url).headers
+            file_size = int(header["Content-Length"])
+            file_name = header["X-File-Name"]
+            print(file_name, f'{(file_size / 1000000):.2f}mb', file_url)
+            section_info_list.append({"file_name": file_name, "file_size": file_size, "file_url": file_url})
+
+    info_dic.update({section_name: section_info_list})
+
+print('Gathering information complete')
+print(20 * '*')
+
+# make dir
+print('Creating folder')
+course_path = Path() / 'resource' / course_title
+if not course_path.exists():
+    course_path.mkdir()
+
+for section in info_dic.keys():
+    dir_path = course_path / section
+    if not dir_path.exists():
+        dir_path.mkdir()
+print('Creating folder complete')
+print(20 * '*')
+
+print('Start downloading...')
+print()
 
 startTime = time.time()
 
-for url in url_list:
+for section in info_dic.keys():
+    for file_info in info_dic[section]:
+        download_link = file_info['file_url']
+        download_name = file_info['file_name']
+        download_size = file_info['file_size']
 
-    headers = {
-        # 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15',
-        # 'Host': 'codewithmosh.com',
-        'Cookie': '_ga=GA1.2.1389156800.1586815101; _gid=GA1.2.348802966.1587774863; ajs_group_id=null; ajs_user_id=30796709; ac_enable_tracking=1; _session_id=ba4a4da3e6b0bbd8a2300332d01be90d; ahoy_visit=76e3c1e0-0909-40be-8f67-edc004be8e9c; signed_in=true; site_preview=logged_in; wistiaVisitorKey=b433d64_3a16f34c-36b6-442c-81ec-c7ebdebbc8f2-10ce11ac0-bab5f04a548b-c7ab; __cfruid=d9f2638715dfbad6424fd01503c4f96b42da8c82-1587774861; ajs_anonymous_id=%22e8defab2-5124-4aff-bf08-421c494e8ec0%22; __cfduid=d676dd83c00058dc896df854a836162e81572048804; _afid=6ec53aa8-63f9-407b-b7c8-e2f160ea8b09; aid=6ec53aa8-63f9-407b-b7c8-e2f160ea8b09; ahoy_visitor=6ec53aa8-63f9-407b-b7c8-e2f160ea8b09',
-        # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-    }
-
-    response = requests.get(url, headers=headers)
-    html = response.text
-
-    soup = BeautifulSoup(html, 'html.parser')
-
-    for link in soup.find_all('a', {'class': 'download'}):
-        download_link = link.get('href')
-        header = requests.head(download_link).headers
-
-        file_name = header["X-File-Name"]
-        file_size = int(header["Content-Length"])
-
-        print(f'Downloading: {file_name}')
+        print(f'Downloading: {download_name}')
         print(f'From url: {download_link}')
-        print(f'File size: {file_size / 1_000_000} mb')
-        path = Path() / 'resource' / 'data_structure' / 'data_structure_1' / file_name
-        path.exists()
+        print(f'File size: {download_size / 1_000_000} mb')
+
+        path = Path() / 'resource' / course_title / section / download_name
 
         start_time = time.time()
         thread_num = 10
 
-        multiThreadDownload(url=download_link, file_path=path, file_size=file_size, thread_num=thread_num)
+        multiThreadDownload(url=download_link, file_path=path, file_size=download_size, thread_num=thread_num)
 
         finish_time = time.time()
         time_download = finish_time - start_time
-        speed = float(file_size) / (1000000.0 * time_download)
+        speed = float(download_size) / (1000000.0 * time_download)
         print('Download complete!')
-        print(f'Download time: {time_download}')
-        print(f'Download speed: {speed}mb/s')
-        print(20 * '-')
+        print(f'Download time: {time_download:.2f}s')
+        print(f'Download speed: {speed:.2f}mb/s')
+        print(10 * '*')
 
 endTime = time.time()
 time = endTime - startTime
